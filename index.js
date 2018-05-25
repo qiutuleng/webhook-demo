a4c226ff49c04c06982faf2af70e9cdad08d76ea
QiuTuleng <qiutuleng@gmail.com>
2018-05-25 15:40:45 +0800

    Define webhook route

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ const app = Express();
 // Routes
 app.get('/', (request, response) => response.send('Hello, world.'));
 
+app.post('/webhook', (request, response) => {
+    console.log('Do something.');
+    response.status(204).send();
+});
+
 app.listen('3000');
